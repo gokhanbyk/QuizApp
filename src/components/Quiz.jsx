31692dcb2167b658f988fd29aa8e622ba410b739
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-04-28 15:18:53 +0300

    QuestionTimer added to quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from "./QuestionTimer.jsx";
 
 function Quiz() {
   // Answers selected by the user
@@ -33,6 +34,7 @@ function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={15000} onTimeout={() => handleSelectAnswer(null)} />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
